f43cf16f5c1ad9f5b34fab91c37bb3f64b061123
DanielFryyEPN <daniel.freire01@epn.edu.ec>
2017-06-03 08:36:33 -0500

    Funciones

---
+++ 05-TypeScript/TypeScript/Functions.js
@@ -0,0 +1,8 @@
+/**
+ * Created by danie on 6/3/2017.
+ */
+function sumar2Numeros(x, y) {
+    if (x === void 0) { x = 12; }
+    return x + y;
+}
+console.log(sumar2Numeros());
---
+++ 05-TypeScript/TypeScript/Functions.ts
@@ -0,0 +1,8 @@
+/**
+ * Created by danie on 6/3/2017.
+ */
+function sumar2Numeros(x: number = 12, y?: number): number {
+    return x + y;
+}
+
+console.log(sumar2Numeros());
