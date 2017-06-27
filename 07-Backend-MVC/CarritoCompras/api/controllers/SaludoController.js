b7b21b55eed2976e293b4294c92e446a589a1144
DanielFryyEPN <daniel.freire01@epn.edu.ec>
2017-06-26 20:50:57 -0500

    conexiones a bases

--- 07-Backend-MVC/CarritoCompras/api/controllers/SaludoController.js
+++ 07-Backend-MVC/CarritoCompras/api/controllers/SaludoController.js
@@ -14,13 +14,21 @@ module.exports = {
             return res.send("Error");
     },
     crearUsuarioQuemado: function (req, res) {
+        var parametros = req.allParams();
         var nuevoUsuario = {
-            nombre: 'Daniel',
-            apellidos: 'Freire',
-            password: '1234',
-            emailAddress: '1@1.com',
-            fechaNacimiento: new Date()
+            nombre: parametros.nombre,
+            apellidos: parametros.apellidos,
+            password: parametros.password,
+            emailAddress: parametros.emailAddress,
+            fechaNacimiento: parametros.fechaNacimiento
         };
+        /*let nuevoUsuario = {
+          nombre: 'Daniel',
+          apellidos: 'Freire',
+          password: '1234',
+          emailAddress: '1@1.com',
+          fechaNacimiento: new Date()
+        };*/
         Usuario.create(nuevoUsuario).exec(function (err, usuarioCreado) {
             if (err)
                 return res.serverError(err);
